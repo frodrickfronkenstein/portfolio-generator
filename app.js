050eefc243ab0ba1cd2a9e7aa5bdbd6c3366d57d
Eric Smith <ericandrewsmith88@gmail.com>
2022-04-16 13:51:28 -0400

    inquirer added

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+    // if (err) throw new Error (err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
