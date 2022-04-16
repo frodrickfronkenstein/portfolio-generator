4fb72f3b2c7d04b6e1a6a92a2592dfc5780849fc
Eric Smith <ericandrewsmith88@gmail.com>
2022-04-16 09:34:40 -0400

    generate webpage added

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-//console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // this
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('==============');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
 
-    console.log('==============');
-    
-    // and this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
