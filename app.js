e2066e3ee4237464cdacf52afe2a0888f2666dbb
Eric Smith <ericandrewsmith88@gmail.com>
2022-04-17 16:27:47 -0400

    template inputs complete

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -139,12 +135,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+         const pageHTML = generatePage(portfolioData);
+
+         fs.writeFile('./index.html', pageHTML, err => {
+             if (err) throw new Error(err);
+
+             console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
-
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-    // if (err) throw new Error (err);
-
-    // console.log('Portfolio complete! Check out index.html to see the output!');
-// });
